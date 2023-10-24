75acc41529789e6b02bec18caf6a35967773a1ad
sfanzalone <sfanzalone@gmail.com>
2023-10-24 07:31:19 -0400

    Added to previously made scripts; created Scoreboard Script
    FloatingScore Script was completed, and now a Scoreboard Script was made, and likely completed.

--- Assets/01-Prospector/__Scripts/FloatingScore.cs
+++ Assets/01-Prospector/__Scripts/FloatingScore.cs
@@ -55,7 +55,7 @@ public class FloatingScore : MonoBehaviour
 
     //Set up the FloatingScore and movement
     //Note the use of parameter defaults for eTimes & eTimeD
-    public void Init(List<Vector2> ePts, float eTimes = 0, float etimesD = 1)
+    public void Init(List<Vector2> ePts, float eTimeS = 0, float eTimeD = 1)
     {
         rectTrans = GetComponent<RectTransform>();
         rectTrans.anchoredPosition = Vector2.zero;
@@ -71,6 +71,100 @@ public class FloatingScore : MonoBehaviour
             transform.position = ePts[0];
 
             return;
+        }
+
+        //If eTimeS is the default, just start at the current time
+        if(eTimeS == 0)
+        {
+            eTimeS = Time.time;            
+        }
+        
+        timeStart = eTimeS;
+        timeDuration = eTimeD;
+
+        state = eFSState.pre; //Set it into pre state, ready to start moving
+    }
+
+    public void FSCallback(FloatingScore fs)
+    {
+        //When this callback is called by SendMessage, add the score from the calling
+        //FloatingScore
+        score += fs.score;
+    }
+
+    //Update is called once per frame
+    private void Update()
+    {
+        //If it's not moving, just return
+        if(state == eFSState.idle)
+        {
+            return;
+        }
+
+        //Get u from the current time and duration u ranges from
+        //0 to 1 (usually)
+        float u = (Time.time - timeStart) / timeDuration;
+
+        //Use Easing class from utils to curve the u value
+        float uC = Easing.Ease(u, easingCurve);
+
+        if(u < 0)
+        {
+            //If u < 0, then we shouldn't move yet.
+            state = eFSState.pre;
+            txt.enabled = false; //Hide the score initially
+        }
+
+        else
+        {
+            if(u >= 1)
+            {
+                //If u >= 1, we're done moving
+                uC = 1;
+
+                //Set uC = 1 so we don't overshoot
+                state = eFSState.post;
+
+                if(reportFinishTo != null)
+                {
+                    //If there's a callback GameObject
+                    //Use SendMessage to call the FSCallback method with this as a parameter
+                    reportFinishTo.SendMessage("FSCallback", this);
+
+                    //Now that the message has been sent,
+                    //Destroy this gameObject
+                    Destroy(gameObject);
+                }
+
+                else
+                {
+                    //If there's nothing to callback
+                    //...then don't destroy this.  Just let it stay still.
+                    state = eFSState.idle;
+                }
+            }
+
+            else
+            {
+                //0 <= u < 1, which means that this is active and moving
+                state = eFSState.active;
+                txt.enabled = true; //Show the score once more
+            }
+        }
+
+        //Use Bezier curve to move this to the right point
+        Vector2 pos = Utils.Bezier(uC, bezierPts);
+
+        //RectTransform anchors can be used to the position UI objects relative
+        //to total size of the screen
+        rectTrans.anchorMin = rectTrans.anchorMax = pos;
+
+        if(fontSizes != null && fontSizes.Count > 0)
+        {
+            //If fontSizes has values in it
+            //...then adjust the fontSize of this GUIText
+            int size = Mathf.RoundToInt(Utils.Bezier(uC, fontSizes));
+            GetComponent<Text>().fontSizes = size;
         }
     }
 }
